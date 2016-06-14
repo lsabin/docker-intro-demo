332ac686301f94d8ce6a968cea9e10e5dfaac7b3
lsabin <sabin.leandro@gmail.com>
2016-06-14 18:39:59 +0200

    cambio el mensaje de saludo.

--- webservice.js
+++ webservice.js
@@ -3,7 +3,7 @@ var server=http.createServer(function(request, response){
        console.log('Knock knock.');
        response.writeHead( 
               200, {"Content-Type": "application/json"});
-       var data = { 'message' : 'Hello Sweetie!!!' };
+       var data = { 'message' : 'Hello you!!!' };
        response.end(JSON.stringify(data) + '\n');
 });
 
